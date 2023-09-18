134d0001b0f0b11ea0d87b4395f186549e41b219
Ruby <ruby.porter@tiscali.co.uk>
2023-09-18 12:28:08 +0100

    adds React Native Paper styling to buttons on home page

--- BotaniBuddy/App.js
+++ BotaniBuddy/App.js
@@ -1,15 +1,29 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { PaperProvider } from 'react-native-paper';
 import HomePage from './src/HomePage';
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  colors: {
+    primary: '#b2d793',
+    secondary: '#f4b393',
+    tertiary: "#58a4b0",
+    onCustomDarkGreen: "#0c7c59",
+    onCustomNavy: "#0b3948",
+  },
+};
+
+
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name='Home Page' component={HomePage} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home Page' component={HomePage} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
   );
 }
--- BotaniBuddy/src/HomePage.jsx
+++ BotaniBuddy/src/HomePage.jsx
@@ -1,13 +1,24 @@
-import { Button, Text, View } from "react-native";
+import {View } from "react-native";
+import { Button, useTheme } from 'react-native-paper';
 import Header from "./Header";
 
+
 export default function HomePage() {
+
+    const theme = useTheme(); 
+
     return (
         <View>
             <Header />
-            <Button title="My Garden"/>
-            <Button title="Daily Tasks"/>
-            <Button title="Find My Plant"/>
+            <Button mode="contained" buttonColor={theme.colors.tertiary}>
+                My Garden
+            </Button>
+            <Button mode="contained" buttonColor={theme.colors.tertiary}>
+                Daily Tasks
+            </Button>
+            <Button  mode="contained" buttonColor={theme.colors.tertiary}>
+                 Find My Plant
+            </Button>
         </View>
     )
 }
