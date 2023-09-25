623a242f9318ca8515e136313b0a9d35824f764a
rubyrubyruby02 <ruby.porter@tiscali.co.uk>
2023-09-25 12:17:49 +0100

    Delete utils directory

--- utils/utils.js
+++
@@ -1,9 +0,0 @@
-exports.logIn = (completeForm) =>{
-    axios.post("URL", completeForm)
-    .then((response)=>{
-        console.log(response);
-    })
-    .catch((error)=>{
-        console.log(error);
-    })
-}
