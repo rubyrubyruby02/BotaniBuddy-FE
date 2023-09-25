9fc48594bc29a8963c450f563b5c4bc8cc83e2f0
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-25 16:17:43 +0100

    add api call for plant identification

--- BotaniBuddy/utils/api.js
+++ BotaniBuddy/utils/api.js
@@ -1,13 +1,26 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "tbc",
+  baseURL: "https://creepy-gray-magpie.cyclic.cloud/api"/* "http://192.168.1.226:3000/api" */,
 });
-
-export function postImage(image, user_id) {
+//https://creepy-gray-magpie.cyclic.cloud/api/users/650da470f65780777749fea5/identify_plants_image
+export function postImage(data, user_id) {
+  // dev: 650da8f0eb4e280152239ead
+  // real: 650da470f65780777749fea5
   return axiosInstance
-    .post(`/api/users/${user_id}/plants/addByImage`, image)
-    .then((response) => {
-      //some stuff
+    .post("/users/650da470f65780777749fea5/identify_plants_image", data, {
+      headers: {
+        "Content-Type": `multipart/form-data`,
+      },
+    })
+    .then(({data}) => {
+      return data
+    })
+    .catch((err) => {
+      for (const key in err) {
+        console.log(`${key}: ${err[key]}`);
+      }
+      console.log((err.response.data.msg));
+      console.log((err.response.data.detail))
     });
 }
