e7313e1154277700d4ab1d61c521fe15863e5eaf
chrisRduckworth <c_duckworth@hotmail.com>
2023-09-28 14:58:49 +0100

    change cyclic url

--- BotaniBuddy/utils/api.js
+++ BotaniBuddy/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: `https://creepy-gray-magpie.cyclic.cloud/`,
+  baseURL: `https://tame-blue-horse-tutu.cyclic.cloud/`,
   headers: {},
 });
 
